ccaceaeacb3555c28b2b091e4707fb362a47a7d0
yxr <935488599@qq.com>
2023-04-18 10:43:42 +0800

    更新

--- AI+X/config.py
+++ AI+X/config.py
@@ -3,9 +3,9 @@ SECRET_KEY = 'adsdadadas'
 # 数据库配置
 HOSTNAME = '127.0.0.1'
 PORT = 3306
-USERNAME = 'root'
-PASSWORD = '1234'
-DATABASE = 'flask_db2'
+USERNAME = '数据库用户名'
+PASSWORD = '数据库密码'
+DATABASE = '数据库名字'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -15,10 +15,10 @@ MAIL_SERVER = 'smtp.qq.com'
 MAIL_PROT = 465
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
-MAIL_USERNAME = "935488599@qq.com"
-MAIL_PASSWORD = "ixggichnrfujbecb"
-MAIL_DEFAULT_SENDER = "935488599@qq.com"
-# ixggichnrfujbecb
+MAIL_USERNAME = "QQ邮箱"
+MAIL_PASSWORD = "邮箱密码"
+
+
 
 
 ALLOWED_EXTENSIONS = {'mp4'}
